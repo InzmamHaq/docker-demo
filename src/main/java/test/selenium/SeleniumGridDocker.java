a1369ed886950f447dfee56149916de044b613d9
fran <fran@localhost>
2018-11-13 15:35:08 +0530

    third commit

--- src/main/java/test/selenium/SeleniumGridDocker.java
+++ src/main/java/test/selenium/SeleniumGridDocker.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.Augmenter;
